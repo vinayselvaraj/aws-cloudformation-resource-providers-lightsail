1a9dc31a81832972ca8c5cf237590f634cc200bf
Vinay Selvaraj <vinas@amazon.com>
2020-08-02 19:50:27 -0400

    Initial commit

---
+++ src/main/java/software/amazon/lightsail/cfn/instance/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.lightsail.cfn.instance;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-lightsail-instance.json");
+    }
+}
